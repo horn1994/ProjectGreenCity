3d224813002f32fa5cd7fef37eb57c5160b282c4
horn1994 <42172218+horn1994@users.noreply.github.com>
2020-06-09 12:49:11 +0200

    update app.py

--- app.py
+++ app.py
@@ -5,21 +5,28 @@ import geopandas
 import pandas as pd
 import folium
 
-from data_processing import gdf_zold, gdf_erdo, gdf_erdopoint
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+import plotly.express as px
 
-maps = folium.Map(location = [47.49909, 19.03093], tiles='Stamen Toner', zoom_start = 12, min_zoom = 11)
+from dash.dependencies import Input, Output
+
+from data_processing import gdf_zold, gdf_erdo, gdf_erdopoint,gdf_combined, gdf_combined_filtered, gdf_all, gdf_aggregated, gdf_combined_aggregated_filtered
+
+maps = folium.Map(location = [47.49909, 19.03093], tiles='Stamen Toner', zoom_start = 12, min_zoom = 11.5)
 
 style_function_zold = lambda x: {
-    'fillColor': 'blue',
+    'fillColor': 'darkblue',
     'color': 'black',
-    'weight': 1.5,
+    'weight': 1,
     'fillOpacity': 0.7
 }
 
 style_function_erdo = lambda x: {
     'fillColor': 'darkgreen',
     'color': 'black',
-    'weight': 1.5,
+    'weight': 1,
     'fillOpacity': 0.7
 }
 
@@ -36,41 +43,133 @@ tooltip_erdo = folium.GeoJsonTooltip(
     )
 
 tooltip_erdopoint = folium.GeoJsonTooltip(
-        fields=['Nev','Kerület', 'tipus'],
+        fields=['Nev','kerulet', 'tipus'],
         aliases=['Név:', 'Kerület:', 'Típus:'],
         localize=True
     )
 
 highlight_function=lambda x: {'weight':2.5,'color':'yellow', 'dashArray':'1'}
-gjson_zold = folium.GeoJson(gdf_zold, style_function_zold, highlight_function, tooltip = tooltip_zold).add_to(maps)
-gjson_erdo = folium.GeoJson(gdf_erdo, style_function_erdo, highlight_function, tooltip = tooltip_erdo).add_to(maps)
+
 gjson_erdo = folium.GeoJson(gdf_erdo, style_function_erdo).add_to(maps)
-gjson_erdopoint = folium.GeoJson(gdf_erdopoint, tooltip =tooltip_erdopoint).add_to(maps)
+gjson_erdopoint = folium.GeoJson(gdf_all, tooltip =tooltip_erdopoint).add_to(maps)
+gjson_zold = folium.GeoJson(gdf_zold, style_function_zold, highlight_function, tooltip = tooltip_zold).add_to(maps)
 
 #maps.save("budapest_zold.html")
 html_string = maps.get_root().render()
+
+#create figures
+figure1 = px.bar(
+    gdf_combined_filtered,
+    x=gdf_combined_filtered['Vérmezõ'].index,
+    y = gdf_combined_filtered['Vérmezõ']
+)
+
+figure2 = px.bar(
+    gdf_combined_aggregated_filtered,
+    x=gdf_combined_aggregated_filtered['Vérmezõ'].index,
+    y = gdf_combined_aggregated_filtered['Vérmezõ']
+)
 #create APP
-
-app = dash.Dash(__name__)
-
-
-app.layout = html.Div([
-    html.H1('Budapest zöldterületei és erdői'),
-    html.Iframe(id='map', srcDoc=html_string, width='50%', height='600'),
-    #html.Button(id='map-submit-button', n_clicks=0, children='Submit')
-])
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
+app.layout = html.Div( #style = {'backgroundColor': 'grey'},
+	children = [ 
+		html.H1('Budapest zöldterületei és erdői',style={'font-family':'Times New Roman'}), 
+    	dcc.Dropdown(
+    		id= 'park_name', 
+    		options=[{"label": e, "value": e} for e in gdf_combined["Nev"].unique()],
+    		placeholder = 'Válaszz egy parkot',
+    	),
+    	html.Div(
+    		children =[
+    					html.Iframe(
+    							id='map', 
+    							srcDoc=html_string, 
+    							width = '100%',
+    							height = 550,
+     			 				style={'marginLeft': 0, 
+    			 					'marginRight': 10, 
+    			 					'marginTop': 10, 
+    			 					'marginBottom': 10,
+    			 					'border': 'none'}, 
+    			 				)
+    						]
+    					),
+    	html.Div(
+    		className = "row",
+    		children = [
+    			html.Div(
+    				className="six columns",
+    				children =[
+    					dcc.Graph(
+    			 				id = 'plot2', 
+    			 				figure = figure2,
+    			 				style={'marginLeft': 0, 
+    			 					'marginRight': 0, 
+    			 					'marginTop': 10, 
+    			 					'marginBottom': 10,
+    			 					'width': 800,
+    			 					'height': 600},							
+    						),
+    				], 
+    			),
+    			 html.Div(
+    			 	className="six columns",
+    			 	children = [
+    			 		dcc.Graph(
+    			 				id = 'plot1', 
+    			 				figure = figure1,
+    			 				style={'marginLeft': 0, 
+    			 					'marginRight': 0, 
+    			 					'marginTop': 10, 
+    			 					'marginBottom': 10,
+    			 					'width': 800,
+    			 					'height': 600},
+    			 			),
+    			 		],
+    			 	),
+    			]
+    		),
+    	]
+	)
+
+### ezt még megkell oldani, hogy mindkétt callback működjön
 # @app.callback(
-#     dash.dependencies.Output('map', 'srcDoc'),
-#     [dash.dependencies.Input('map-submit-button', 'n_clicks')])
-# def update_map(n_clicks):
-#     if n_clicks is None:
-#         return dash.no_update
-#     else:
-#     	return open('budapest_zold.html', 'r').read()
+#     dash.dependencies.Output("plot", "figure"),
+#     [dash.dependencies.Input("park_name", "value")],
+# )
+@app.callback(
+	[Output('map','srcDoc'),
+	Output("plot2", "figure"),
+	Output("plot1", "figure")],
+    [Input('park_name', 'value')],
+
+)
+
+
+def update_map(value):
+	if (value is None) or (value == "Válassz egy parkot"):
+		bp_map = maps.get_root().render()
+		fig = px.bar(gdf_combined_filtered, x=gdf_combined_filtered.index, y = gdf_combined_filtered['Vérmezõ'])
+		fig.update_layout(yaxis_title="Vérmezõ")
+		fig2 = px.bar(gdf_combined_aggregated_filtered, x=gdf_combined_aggregated_filtered.index, y = gdf_combined_aggregated_filtered['Vérmezõ'])
+		fig2.update_layout(yaxis_title="Vérmezõ")
+		return bp_map, fig, fig2
+	else:
+		bp_map = folium.Map(location = [gdf_all[gdf_all['Nev'] == value].centroid_lat, gdf_all[gdf_all['Nev'] == value].centroid_lon], tiles='Stamen Toner', zoom_start = 17, min_zoom = 11.5)
+		gjson_erdo = folium.GeoJson(gdf_erdo, style_function_erdo).add_to(bp_map)
+		gjson_erdopoint = folium.GeoJson(gdf_all, tooltip =tooltip_erdopoint).add_to(bp_map)
+		gjson_zold = folium.GeoJson(gdf_zold, style_function_zold, highlight_function, tooltip = tooltip_zold).add_to(bp_map)
+		bp_map = bp_map.get_root().render()
+		filt_df = gdf_combined_filtered.loc[:, gdf_combined_filtered.columns.isin([value])]
+		fig = px.bar(filt_df, x=filt_df.index, y = filt_df[value])
+		filt_df2 = gdf_combined_aggregated_filtered.loc[:, gdf_combined_aggregated_filtered.columns.isin([value])]
+		fig2 = px.bar(filt_df2, x=filt_df2.index, y = filt_df2[value])		
+		return bp_map, fig, fig2
+
 
 server = app.server
-
 if __name__ == '__main__':
     app.run_server(debug=True)
--- data_processing.py
+++ data_processing.py
@@ -5,20 +5,91 @@ import folium
 import shapely.geometry
 from simpledbf import Dbf5
 from shapely import wkt
+from datetime import date
 
 
-
-gdf_zold = pd.read_csv("https://raw.githubusercontent.com/horn1994/ProjectGreenCity/master/test_data/zoldter_pg_region.csv")
+gdf_zold = pd.read_csv(r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\zoldter_pg_region.csv")
 gdf_zold['geometry'] = gdf_zold['geometry'].apply(wkt.loads)
 gdf_zold = geopandas.GeoDataFrame(gdf_zold)
 gdf_zold.set_geometry('geometry', crs={'init': u'epsg:'+str(4326)}, inplace=True)
-gdf_erdo = pd.read_csv("https://raw.githubusercontent.com/horn1994/ProjectGreenCity/master/test_data/erdo_pg_region.csv")
+gdf_erdo = pd.read_csv(r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\erdo_pg_region.csv")
 gdf_erdo['geometry'] = gdf_erdo['geometry'].apply(wkt.loads)
 gdf_erdo = geopandas.GeoDataFrame(gdf_erdo)
 gdf_erdo.set_geometry('geometry', crs={'init': u'epsg:'+str(4326)}, inplace=True)
-gdf_erdopoint = pd.read_csv("https://raw.githubusercontent.com/horn1994/ProjectGreenCity/master/test_data/erdo_pt_point.csv")
+gdf_erdopoint = pd.read_csv(r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\erdo_pt_point.csv")
 gdf_erdopoint['geometry'] = gdf_erdopoint['geometry'].apply(wkt.loads)
 gdf_erdopoint = geopandas.GeoDataFrame(gdf_erdopoint)
 gdf_erdopoint.set_geometry('geometry', crs={'init': u'epsg:'+str(4326)}, inplace=True)
+gdf_erdopoint.rename(columns = {'Kerület': 'kerulet'}, inplace = True)
+#gdf_erdopoint.drop('Unnamed: 0', axis = 1, inplace = True)
+#gdf_zold.drop('Unnamed: 0', axis = 1, inplace = True)
+gdf_all = gdf_zold.append(gdf_erdopoint)
 
 
+def create_data(gdf_all):
+    start_date = date.fromisoformat("2020-06-06")  # this is the day of the first data
+    today = date.today()
+    day_since_start = (today - start_date).days
+    new_data = pd.read_excel(
+        r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
+            day_since_start
+        )
+    )
+    gdf_combined = pd.merge(gdf_all, new_data, on=["Nev", "kerulet"], how="outer")
+    gdf_combined["mean_visitors"] = gdf_combined.iloc[:, 10:].mean(axis=1).round(0)
+    # gdf_combined["mean_visitors"] = gdf_combined["mean_visitors"]
+    gdf_combined_filtered = (
+        gdf_combined.set_index("Nev")
+        .iloc[:, 10:-1]
+        .rename(lambda x: str(x) + " óra", axis=1)
+        .T
+    )
+
+    return gdf_combined, gdf_combined_filtered
+
+
+gdf_combined, gdf_combined_filtered = create_data(gdf_all)
+
+## TRY EXCEPTET MEGCSINÁLNI, HOGY 30 NAPNYI AVERAGE LEGYEN
+def create_agg_data(gdf_all):
+    start_date = date.fromisoformat("2020-06-06")  # this is the day of the first data
+    today = date.today()
+    day_since_start = (today - start_date).days
+    new_data = pd.read_excel(
+        r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
+            day_since_start
+        )
+    )
+    gdf_aggregated = new_data.iloc[:, 4:]
+    for data in range(1, day_since_start):
+        read_new = pd.read_excel(
+            r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
+                data
+            )
+        )
+        gdf_aggregated += round(read_new.iloc[:, 4:])
+
+    gdf_aggregated = round(gdf_aggregated / day_since_start)
+    gdf_aggregated = pd.concat(
+        [
+            new_data.iloc[:, :4].reset_index(drop=True),
+            gdf_aggregated.reset_index(drop=True),
+        ],
+        axis=1,
+    )
+    gdf_combined_aggregated = pd.merge(
+        gdf_all, gdf_aggregated, on=["Nev", "kerulet"], how="outer"
+    )
+    gdf_combined_aggregated["mean_visitors"] = (
+        gdf_combined_aggregated.iloc[:, 10:].mean(axis=1).round(0)
+    )
+    gdf_combined_aggregated_filtered = (
+        gdf_combined_aggregated.set_index("Nev")
+        .iloc[:, 10:-1]
+        .rename(lambda x: str(x) + " óra", axis=1)
+        .T
+    )
+    return gdf_aggregated, gdf_combined_aggregated_filtered
+
+
+gdf_aggregated, gdf_combined_aggregated_filtered = create_agg_data(gdf_all)
