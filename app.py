628cb259c8993424947f0c143b09ff23b46ff8a4
horn1994 <42172218+horn1994@users.noreply.github.com>
2020-06-12 15:21:11 +0200

    update 2020.06.12.

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
 import plotly.express as px
-
+import plotly.graph_objects as go
 from dash.dependencies import Input, Output
 
-from data_processing import gdf_zold, gdf_erdo, gdf_erdopoint,gdf_combined, gdf_combined_filtered, gdf_all, gdf_aggregated, gdf_combined_aggregated_filtered
+from data_processing import gdf_zold, gdf_erdo, gdf_erdopoint,gdf_combined, gdf_combined_filtered, gdf_all, gdf_aggregated, gdf_combined_aggregated, gdf_combined_aggregated_filtered
 
-maps = folium.Map(location = [47.49909, 19.03093], tiles='Stamen Toner', zoom_start = 12, min_zoom = 11.5)
+maps = folium.Map(location = [47.49909, 19.03093], tiles='Stamen Toner', zoom_start = 12, min_zoom = 11)
 
 style_function_zold = lambda x: {
     'fillColor': 'darkblue',
@@ -31,8 +31,8 @@ style_function_erdo = lambda x: {
 }
 
 tooltip_zold = folium.GeoJsonTooltip(
-        fields=['Nev','kerulet', 'tipus'],
-        aliases=['Név:', 'Kerület:', 'Típus:'],
+        fields=['Nev','kerulet', 'tipus', 'mean_visitors'],
+        aliases=['Név:', 'Kerület:', 'Típus:', 'Átlagos látogatószám (heti):'],
         localize=True
     )
 
@@ -42,26 +42,51 @@ tooltip_erdo = folium.GeoJsonTooltip(
         localize=True
     )
 
-tooltip_erdopoint = folium.GeoJsonTooltip(
-        fields=['Nev','kerulet', 'tipus'],
-        aliases=['Név:', 'Kerület:', 'Típus:'],
-        localize=True
-    )
+# tooltip_erdopoint = folium.GeoJsonTooltip(
+#         fields=['Nev','kerulet', 'tipus'],
+#         aliases=['Név:', 'Kerület:', 'Típus:'],
+#         localize=True
+#     )
 
 highlight_function=lambda x: {'weight':2.5,'color':'yellow', 'dashArray':'1'}
 
+#gjson_erdo = folium.GeoJson(gdf_erdo, style_function_erdo, highlight_function, tooltip = tooltip_erdo).add_to(maps)
 gjson_erdo = folium.GeoJson(gdf_erdo, style_function_erdo).add_to(maps)
-gjson_erdopoint = folium.GeoJson(gdf_all, tooltip =tooltip_erdopoint).add_to(maps)
-gjson_zold = folium.GeoJson(gdf_zold, style_function_zold, highlight_function, tooltip = tooltip_zold).add_to(maps)
+gjson_erdopoint = folium.GeoJson(gdf_combined_aggregated,style_function_zold, tooltip =tooltip_zold).add_to(maps)
+#gjson_zold = folium.GeoJson(gdf_zold, style_function_zold, highlight_function, tooltip = tooltip_zold).add_to(maps)
 
 #maps.save("budapest_zold.html")
 html_string = maps.get_root().render()
 
 #create figures
-figure1 = px.bar(
-    gdf_combined_filtered,
-    x=gdf_combined_filtered['Vérmezõ'].index,
-    y = gdf_combined_filtered['Vérmezõ']
+figure1 = go.Figure(
+    data=[
+        go.Bar(
+            name="Átlagos heti látogatószám",
+            x=gdf_combined_aggregated_filtered["Vérmezõ"].index,
+            y=gdf_combined_aggregated_filtered["Vérmezõ"],
+        ),
+        go.Bar(
+            name="Előző napi látogató szám",
+            x=gdf_combined_filtered["Vérmezõ"].index,
+            y=gdf_combined_filtered["Vérmezõ"],
+        ),
+    ]
+)
+
+figure1.update_layout(
+    title={'text':"Látogatók számának előző napi és átlagos előző heti óránkénti eloszlása",'y':1,
+        'x':0.5,
+           'xanchor': 'center'},
+    xaxis_title="Óránkénti látogatószám",
+    yaxis_title="Vérmezõ",
+    font=dict(
+        #family="Courier New, monospace",
+        size=12,
+        #color="#7f7f7f"
+    ),
+    plot_bgcolor='rgba(0,0,0,0)',
+    legend = dict(x = 0, y = 1)
 )
 
 figure2 = px.bar(
@@ -93,7 +118,8 @@ app.layout = html.Div( #style = {'backgroundColor': 'grey'},
     			 					'marginRight': 10, 
     			 					'marginTop': 10, 
     			 					'marginBottom': 10,
-    			 					'border': 'none'}, 
+    			 					#'border': 'none'
+    			 					}, 
     			 				)
     						]
     					),
@@ -152,22 +178,78 @@ app.layout = html.Div( #style = {'backgroundColor': 'grey'},
 def update_map(value):
 	if (value is None) or (value == "Válassz egy parkot"):
 		bp_map = maps.get_root().render()
-		fig = px.bar(gdf_combined_filtered, x=gdf_combined_filtered.index, y = gdf_combined_filtered['Vérmezõ'])
-		fig.update_layout(yaxis_title="Vérmezõ")
+		figure1 = go.Figure(
+		    data=[
+		        go.Bar(
+		            name="Átlagos heti látogatószám",
+		            x=gdf_combined_aggregated_filtered["Vérmezõ"].index,
+		            y=gdf_combined_aggregated_filtered["Vérmezõ"],
+		        ),
+		        go.Bar(
+		            name="Előző napi látogató szám",
+		            x=gdf_combined_filtered["Vérmezõ"].index,
+		            y=gdf_combined_filtered["Vérmezõ"],
+		        ),
+		    ]
+		)
+
+		figure1.update_layout(
+		    title={'text':"Látogatók számának előző napi és átlagos előző heti óránkénti eloszlása",'y':0.88,
+		        'x':0.5,
+		           'xanchor': 'center'},
+		    xaxis_title="Óránkénti látogatószám",
+		    yaxis_title="Vérmezõ",
+		    font=dict(
+		        #family="Courier New, monospace",
+		        size=12,
+		        #color="#7f7f7f"
+		    ),
+		    plot_bgcolor='rgba(0,0,0,0)',
+		    legend = dict(x = 0, y = 1)
+		)
 		fig2 = px.bar(gdf_combined_aggregated_filtered, x=gdf_combined_aggregated_filtered.index, y = gdf_combined_aggregated_filtered['Vérmezõ'])
 		fig2.update_layout(yaxis_title="Vérmezõ")
-		return bp_map, fig, fig2
+		return bp_map, figure1, fig2
 	else:
 		bp_map = folium.Map(location = [gdf_all[gdf_all['Nev'] == value].centroid_lat, gdf_all[gdf_all['Nev'] == value].centroid_lon], tiles='Stamen Toner', zoom_start = 17, min_zoom = 11.5)
 		gjson_erdo = folium.GeoJson(gdf_erdo, style_function_erdo).add_to(bp_map)
-		gjson_erdopoint = folium.GeoJson(gdf_all, tooltip =tooltip_erdopoint).add_to(bp_map)
-		gjson_zold = folium.GeoJson(gdf_zold, style_function_zold, highlight_function, tooltip = tooltip_zold).add_to(bp_map)
+		gjson_erdopoint = folium.GeoJson(gdf_combined_aggregated,style_function_zold, tooltip =tooltip_zold).add_to(bp_map)
 		bp_map = bp_map.get_root().render()
 		filt_df = gdf_combined_filtered.loc[:, gdf_combined_filtered.columns.isin([value])]
-		fig = px.bar(filt_df, x=filt_df.index, y = filt_df[value])
-		filt_df2 = gdf_combined_aggregated_filtered.loc[:, gdf_combined_aggregated_filtered.columns.isin([value])]
+		filt_df2 = gdf_combined_aggregated_filtered.loc[:, gdf_combined_aggregated_filtered.columns.isin([value])]		
+		#fig = px.bar(filt_df, x=filt_df.index, y = filt_df[value])
+		figure1 = go.Figure(
+		    data=[
+		        go.Bar(
+		            name="Átlagos heti látogatószám",
+		            x=filt_df2.index,
+		            y=filt_df2[value],
+		        ),
+		        go.Bar(
+		            name="Előző napi látogató szám",
+		            x=filt_df.index,
+		            y=filt_df[value],
+		        ),
+		    ]
+		)
+
+		figure1.update_layout(
+		    title={'text':"Látogatók számának előző napi és átlagos előző heti óránkénti eloszlása",'y':0.88,
+		        'x':0.5,
+		           'xanchor': 'center'},
+		    xaxis_title="Óránkénti látogatószám",
+		    yaxis_title=value,
+		    font=dict(
+		        #family="Courier New, monospace",
+		        size=12,
+		        #color="#7f7f7f"
+		    ),
+		    plot_bgcolor='rgba(0,0,0,0)',
+		    legend = dict(x = 0, y = 1)
+		)
+
 		fig2 = px.bar(filt_df2, x=filt_df2.index, y = filt_df2[value])		
-		return bp_map, fig, fig2
+		return bp_map, figure1, fig2
 
 
 server = app.server
--- data_processing.py
+++ data_processing.py
@@ -26,17 +26,99 @@ gdf_erdopoint.rename(columns = {'Kerület': 'kerulet'}, inplace = True)
 gdf_all = gdf_zold.append(gdf_erdopoint)
 
 
+# def create_data(gdf_all):
+#     start_date = date.fromisoformat("2020-06-06")  # this is the day of the first data
+#     today = date.today()
+#     day_since_start = (today - start_date).days
+#     new_data = pd.read_excel(
+#         r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
+#             day_since_start
+#         )
+#     )
+#     gdf_combined = pd.merge(gdf_all, new_data, on=["Nev", "kerulet"], how="outer")
+#     gdf_combined["mean_visitors"] = gdf_combined.iloc[:, 10:].mean(axis=1).round(0)
+#     # gdf_combined["mean_visitors"] = gdf_combined["mean_visitors"]
+#     gdf_combined_filtered = (
+#         gdf_combined.set_index("Nev")
+#         .iloc[:, 10:-1]
+#         .rename(lambda x: str(x) + " óra", axis=1)
+#         .T
+#     )
+
+#     return gdf_combined, gdf_combined_filtered
+
+
+# gdf_combined, gdf_combined_filtered = create_data(gdf_all)
+
+# ## TRY EXCEPTET MEGCSINÁLNI, HOGY 30 NAPNYI AVERAGE LEGYEN
+# def create_agg_data(gdf_all):
+#     start_date = date.fromisoformat("2020-06-06")  # this is the day of the first data
+#     today = date.today()
+#     day_since_start = (today - start_date).days
+#     new_data = pd.read_excel(
+#         r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
+#             day_since_start
+#         )
+#     )
+#     gdf_aggregated = new_data.iloc[:, 4:]
+#     for data in range(1, day_since_start):
+#         read_new = pd.read_excel(
+#             r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
+#                 data
+#             )
+#         )
+#         gdf_aggregated += round(read_new.iloc[:, 4:])
+
+#     gdf_aggregated = round(gdf_aggregated / day_since_start)
+#     gdf_aggregated = pd.concat(
+#         [
+#             new_data.iloc[:, :4].reset_index(drop=True),
+#             gdf_aggregated.reset_index(drop=True),
+#         ],
+#         axis=1,
+#     )
+#     gdf_combined_aggregated = pd.merge(
+#         gdf_all, gdf_aggregated, on=["Nev", "kerulet"], how="outer"
+#     )
+#     gdf_combined_aggregated["mean_visitors"] = (
+#         gdf_combined_aggregated.iloc[:, 10:].mean(axis=1).round(0)
+#     )
+#     gdf_combined_aggregated_filtered = (
+#         gdf_combined_aggregated.set_index("Nev")
+#         .iloc[:, 10:-1]
+#         .rename(lambda x: str(x) + " óra", axis=1)
+#         .T
+#     )
+#     return gdf_aggregated, gdf_combined_aggregated_filtered
+
+
+# gdf_aggregated, gdf_combined_aggregated_filtered = create_agg_data(gdf_all)
+
+
+
+from datetime import date
+
+
 def create_data(gdf_all):
-    start_date = date.fromisoformat("2020-06-06")  # this is the day of the first data
+    
+    start_date = date.fromisoformat("2020-06-05")  # this is the day of the first data
     today = date.today()
     day_since_start = (today - start_date).days
-    new_data = pd.read_excel(
-        r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
-            day_since_start
-        )
-    )
+    
+    load = False
+    while load == False:
+        try:
+            new_data = pd.read_excel(
+                r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
+                    day_since_start
+                )
+            )
+            load = True
+        except:
+            day_since_start -= 1
+
     gdf_combined = pd.merge(gdf_all, new_data, on=["Nev", "kerulet"], how="outer")
-    gdf_combined["mean_visitors"] = gdf_combined.iloc[:, 10:].mean(axis=1).round(0)
+    gdf_combined["mean_visitors"] = gdf_combined.iloc[:, 11:].mean(axis=1).round(0)
     # gdf_combined["mean_visitors"] = gdf_combined["mean_visitors"]
     gdf_combined_filtered = (
         gdf_combined.set_index("Nev")
@@ -50,24 +132,38 @@ def create_data(gdf_all):
 
 gdf_combined, gdf_combined_filtered = create_data(gdf_all)
 
-## TRY EXCEPTET MEGCSINÁLNI, HOGY 30 NAPNYI AVERAGE LEGYEN
+
 def create_agg_data(gdf_all):
-    start_date = date.fromisoformat("2020-06-06")  # this is the day of the first data
+    
+    start_date = date.fromisoformat("2020-06-05")  # this is the day of the first data
     today = date.today()
     day_since_start = (today - start_date).days
-    new_data = pd.read_excel(
-        r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
-            day_since_start
-        )
-    )
-    gdf_aggregated = new_data.iloc[:, 4:]
-    for data in range(1, day_since_start):
-        read_new = pd.read_excel(
-            r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
-                data
+    
+    missing_file_count = 0
+    load = False
+    while load == False:
+        try:
+            new_data = pd.read_excel(
+                r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
+                    day_since_start
+                )
             )
-        )
-        gdf_aggregated += round(read_new.iloc[:, 4:])
+            load = True
+        except FileNotFoundError:
+            missing_file_count += 1
+            day_since_start -= 1
+            
+    gdf_aggregated = new_data.iloc[:, 4:]
+    for data in range(day_since_start-7, day_since_start): #1-6 ig megy, így a legújabb napnak nincs kétszeres súlya 
+        try:
+            read_new = pd.read_excel(
+                r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
+                    data
+                )
+            )
+            gdf_aggregated += round(read_new.iloc[:, 4:])
+        except FileNotFoundError:
+            pass
 
     gdf_aggregated = round(gdf_aggregated / day_since_start)
     gdf_aggregated = pd.concat(
@@ -81,7 +177,7 @@ def create_agg_data(gdf_all):
         gdf_all, gdf_aggregated, on=["Nev", "kerulet"], how="outer"
     )
     gdf_combined_aggregated["mean_visitors"] = (
-        gdf_combined_aggregated.iloc[:, 10:].mean(axis=1).round(0)
+        gdf_combined_aggregated.iloc[:, 11:].mean(axis=1).round(0)
     )
     gdf_combined_aggregated_filtered = (
         gdf_combined_aggregated.set_index("Nev")
@@ -89,7 +185,7 @@ def create_agg_data(gdf_all):
         .rename(lambda x: str(x) + " óra", axis=1)
         .T
     )
-    return gdf_aggregated, gdf_combined_aggregated_filtered
+    return gdf_aggregated, gdf_combined_aggregated, gdf_combined_aggregated_filtered
 
 
-gdf_aggregated, gdf_combined_aggregated_filtered = create_agg_data(gdf_all)
+gdf_aggregated,gdf_combined_aggregated, gdf_combined_aggregated_filtered = create_agg_data(gdf_all)
