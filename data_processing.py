e48baaa82c6004d8d5132aed3fae9d488fc39743
horn1994 <42172218+horn1994@users.noreply.github.com>
2020-06-20 14:46:09 +0200

    could stroage update

--- data_processing.py
+++ data_processing.py
@@ -6,17 +6,39 @@ import shapely.geometry
 from simpledbf import Dbf5
 from shapely import wkt
 from datetime import date
+import dropbox
+import io
 
 
-gdf_zold = pd.read_csv(r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\zoldter_pg_region.csv")
+token = "tMyiUS6n5NAAAAAAAAAADHpC4VE5Etu-fs3_EBIQDOikg6c6KR8RwF_QTWgMsjYY"
+DBX = dropbox.Dropbox(token)
+#Zoldteruletek
+_, res = DBX.files_download("/polygons/zoldter_pg_region.csv")
+
+with io.BytesIO(res.content) as stream:
+    gdf_zold = pd.read_csv(stream, index_col=0)
+    
 gdf_zold['geometry'] = gdf_zold['geometry'].apply(wkt.loads)
 gdf_zold = geopandas.GeoDataFrame(gdf_zold)
 gdf_zold.set_geometry('geometry', crs={'init': u'epsg:'+str(4326)}, inplace=True)
-gdf_erdo = pd.read_csv(r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\erdo_pg_region.csv")
+
+#Erdok
+_, res = DBX.files_download("/polygons/erdo_pg_region.csv")
+
+with io.BytesIO(res.content) as stream:
+    gdf_erdo = pd.read_csv(stream, index_col=0)
+
 gdf_erdo['geometry'] = gdf_erdo['geometry'].apply(wkt.loads)
 gdf_erdo = geopandas.GeoDataFrame(gdf_erdo)
 gdf_erdo.set_geometry('geometry', crs={'init': u'epsg:'+str(4326)}, inplace=True)
-gdf_erdopoint = pd.read_csv(r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\erdo_pt_point.csv")
+
+#Erdok, adatokkal
+_, res = DBX.files_download("/polygons/erdo_pt_point.csv")
+
+with io.BytesIO(res.content) as stream:
+    gdf_erdopoint = pd.read_csv(stream, index_col=0)
+    
+
 gdf_erdopoint['geometry'] = gdf_erdopoint['geometry'].apply(wkt.loads)
 gdf_erdopoint = geopandas.GeoDataFrame(gdf_erdopoint)
 gdf_erdopoint.set_geometry('geometry', crs={'init': u'epsg:'+str(4326)}, inplace=True)
@@ -34,17 +56,16 @@ def create_data(gdf_all):
     load = False
     while load == False:
         try:
-            new_data = pd.read_excel(
-                r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
-                    day_since_start
-                )
-            )
+            _, res = DBX.files_download("/data/test{}.csv".format(day_since_start))
+
+            with io.BytesIO(res.content) as stream:
+                new_data = pd.read_csv(stream, sep = ";", header = 0, encoding = "UTF-8", index_col=0)
             load = True
         except:
             day_since_start -= 1
 
     gdf_combined = pd.merge(gdf_all, new_data, on=["Nev", "kerulet"], how="outer")
-    gdf_combined["mean_visitors"] = gdf_combined.iloc[:, 11:].mean(axis=1).round(0)
+    gdf_combined["mean_visitors"] = gdf_combined.iloc[:, 8:].mean(axis=1).round(0)
     # gdf_combined["mean_visitors"] = gdf_combined["mean_visitors"]
     gdf_combined_filtered = (
         gdf_combined.set_index("Nev")
@@ -69,26 +90,26 @@ def create_agg_data(gdf_all):
     load = False
     while load == False:
         try:
-            new_data = pd.read_excel(
-                r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
-                    day_since_start
-                )
-            )
+            _, res = DBX.files_download("/data/test{}.csv".format(day_since_start))
+
+            with io.BytesIO(res.content) as stream:
+                new_data = pd.read_csv(stream, sep = ";", header = 0, encoding = "UTF-8", index_col=0)
+                
             load = True
-        except FileNotFoundError:
+        except:
             missing_file_count += 1
             day_since_start -= 1
             
     gdf_aggregated = new_data.iloc[:, 4:]
     for data in range(day_since_start-7, day_since_start): #1-6 ig megy, így a legújabb napnak nincs kétszeres súlya 
         try:
-            read_new = pd.read_excel(
-                r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
-                    data
-                )
-            )
+            _, res = DBX.files_download("/data/test{}.csv".format(data))
+
+            with io.BytesIO(res.content) as stream:
+                read_new = pd.read_csv(stream, sep = ";", header = 0, encoding = "UTF-8", index_col=0)
+                
             gdf_aggregated += round(read_new.iloc[:, 4:])
-        except FileNotFoundError:
+        except:
             pass
 
     gdf_aggregated = round(gdf_aggregated / day_since_start)
@@ -107,7 +128,7 @@ def create_agg_data(gdf_all):
     )
     gdf_combined_aggregated_filtered = (
         gdf_combined_aggregated.set_index("Nev")
-        .iloc[:, 10:-1]
+        .iloc[:, 8:-1]
         .rename(lambda x: str(x) + " óra", axis=1)
         .T
     )
@@ -124,28 +145,28 @@ def figure2():
     load = False
     while load == False:
         try:
-            mean_visitors = pd.read_excel(
-                r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
-                    day_since_start
-                )
-            )
+            _, res = DBX.files_download("/data/test{}.csv".format(day_since_start))
+
+            with io.BytesIO(res.content) as stream:
+                mean_visitors = pd.read_csv(stream, sep = ";", header = 0, encoding = "UTF-8", index_col=0)
+                
             mean_visitors['{}. nap'.format(day_since_start)] = mean_visitors.iloc[:,4:].mean(axis=1).round(0)
             mean_visitors = mean_visitors.set_index('Nev').iloc[:,-1:]
             load = True
-        except FileNotFoundError:
+        except:
             day_since_start -= 1
     for data in range(day_since_start-7, day_since_start): 
         try:
-            read_new = pd.read_excel(
-                r"C:\Users\T440s\Google Drive\rajk\rajkos_kurzusok\prog2\PROJECT\data\test\test{}.xlsx".format(
-                    data
-                )
-            )
+            _, res = DBX.files_download("/data/test{}.csv".format(data))
+
+            with io.BytesIO(res.content) as stream:
+                read_new = pd.read_csv(stream, sep = ";", header = 0, encoding = "UTF-8", index_col=0)
+                
             read_new['{}. nap'.format(data)] = read_new.iloc[:,4:].mean(axis=1).round(0)
             read_new = read_new.set_index('Nev').iloc[:,-1:]
             mean_visitors = mean_visitors.merge(read_new, right_index=True, left_index=True)
             
-        except FileNotFoundError:
+        except:
             pass
     mean_visitors = mean_visitors.T.sort_index()
     return mean_visitors
